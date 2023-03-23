1178e124e7e78ee3307843629987a04dff83b55b
m-e-l-i-s-s-a <57683305+m-e-l-i-s-s-a@users.noreply.github.com>
2023-03-23 09:57:06 -0700

    Started Unit 3

---
+++ Unit3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
