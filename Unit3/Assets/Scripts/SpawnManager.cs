8f6f3c38c3433e6b3e4582d87e3a37b9f49c69ef
m-e-l-i-s-s-a <57683305+m-e-l-i-s-s-a@users.noreply.github.com>
2023-03-31 08:31:23 -0700

    ahhh
    ahhh

--- Unit3/Assets/Scripts/SpawnManager.cs
+++ Unit3/Assets/Scripts/SpawnManager.cs
@@ -6,14 +6,17 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
 
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(20, 0, 0);
+
     private float startDelay = 2;
     private float repeatRate = 2;
 
+    private PlayerController playerControllerScript;
 
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
 
@@ -24,6 +27,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
